2868437695181aee83b53c8f822d90f1168c1aa7
Pamela Stacey Alvarez <p.stacey.a@gmail.com>
2017-02-11 21:59:26 -0500

    fix

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -12,8 +12,7 @@
  * @since Accelerate Marketing 1.0
  */
 
-get_header(); ?>
- <pre><?php print_r($wp_query->posts); exit; ?></pre>
+get_header(); ?> 
 <section class="home-page">
 	<div class="site-content">
 		<?php while ( have_posts() ) : the_post(); ?>
@@ -23,6 +22,19 @@ get_header(); ?>
 			</div>
 		<?php endwhile; // end of the loop. ?>
 	</div><!-- .container -->
-</section><!-- .home-page -->
-
+</section><!-- .home-page --> 
+<section class="recent-posts">
+ <div class="site-content">
+  <div class="blog-post">
+   <h4>From the Blog</h4>
+    <?php query_posts('posts_per_page=1'); ?>
+     <?php while ( have_posts() ) : the_post(); ?>
+       <h2><?php the_title(); ?></h2>
+       <?php the_excerpt(); ?> 
+       <a class="read-more-link" href="<?php the_permalink(); ?>">Read More <span>&rsaquo;</span></a>
+     <?php endwhile; ?> 
+    <?php wp_reset_query(); ?>
+   </div>
+  </div>
+</section>
 <?php get_footer(); ?>
